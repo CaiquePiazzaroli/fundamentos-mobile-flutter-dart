ac960721328c087d2a4f2f743f3f8a58dc1f9d24
caique mendes <caique405@gmail.com>
2025-04-08 08:21:36 -0300

    Exercicios 1 e 2

--- exercicios/ex_aula5/ex01_gridview/lib/main.dart
+++ exercicios/ex_aula5/ex01_gridview/lib/main.dart
@@ -1,56 +1,84 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp(home: Scaffold(body: MyGridView()));
+  }
+}
+
+class MyGridView extends StatelessWidget {
+  const MyGridView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 2,
+      children: <Widget>[
+        GridViewItem(
+          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+          'Coruja',
+        ),
+        GridViewItem(
+          'https://images.pexels.com/photos/47547/squirrel-animal-cute-rodents-47547.jpeg',
+          'Brown Squirrel',
+        ),
+        GridViewItem(
+          'https://images.pexels.com/photos/792381/pexels-photo-792381.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+          'Tiger',
+        ),
+        GridViewItem(
+          'https://images.pexels.com/photos/325045/pexels-photo-325045.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+          'Siamese Fighting Fish',
+        ),
+      ],
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class GridViewItem extends StatelessWidget {
+  late String linkImage;
+  late String description;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  GridViewItem(this.linkImage, this.description, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(26),
+      child: Container(
+        margin: EdgeInsets.all(6),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(linkImage),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: 20,
+              color: const Color.fromARGB(122, 0, 0, 0),
+              padding: EdgeInsets.symmetric(vertical: 20.0),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 8.0,
+                  horizontal: 20,
+                ),
+                child: Text(description, style: TextStyle(color: Colors.white)),
+              ),
+            ),
+          ],
+        ),
       ),
-      body: Text("Grid view aqui"),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
-
